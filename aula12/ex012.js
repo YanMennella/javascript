7623f48125e76f32c11d58a856c4dc782eb504b6
YanMennella <yancorp.tecdev@gmail.com>
2025-03-21 19:52:22 -0300

    bom dia, boa tarde, boa noite

--- aula12/ex012.js
+++ aula12/ex012.js
@@ -1,9 +1,15 @@
-var hora = 19
+var hora = 5
 console.log(`Agora são exatamente ${hora} horas.`)
-if (hora < 12) {
+if (hora >= 6 && hora < 12) {
     console.log('Bom Dia!')
-} else if (hora <= 18) {
+} else if (hora > 12 && hora <=18) {
     console.log('Boa Tarde!')
+} else if (hora == 12) {
+    console.log('Bom Almoço!')
+} else if (hora > 18 && hora <=23) {
+        console.log('Boa Noite!')
+} else if (hora >= 0 && hora < 6) {
+    console.log('Boa Madrugada!')
 } else {
-    console.log('Boa Noite!')
+    console.log('Hora Inválida!')
 }
