4879379a0906c0be93a68ffee867b8880c93f4e3
YanMennella <yancorp.tecdev@gmail.com>
2025-03-21 20:12:34 -0300

    refinando

--- aula12/ex012.js
+++ aula12/ex012.js
@@ -1,16 +1,14 @@
-var 
-var hora = 5
+var agora = new Date()
+var hora = agora.getHours()
 console.log(`Agora são exatamente ${hora} horas.`)
 if (hora >= 6 && hora < 12) {
     console.log('Bom Dia!')
-} else if (hora > 12 && hora <=18) {
-    console.log('Boa Tarde!')
 } else if (hora == 12) {
     console.log('Bom Almoço!')
+} else if (hora > 12 && hora <=18) {
+    console.log('Boa Tarde!')
 } else if (hora > 18 && hora <=23) {
         console.log('Boa Noite!')
 } else if (hora >= 0 && hora < 6) {
     console.log('Boa Madrugada!')
-} else {
-    console.log('Hora Inválida!')
 }
