a7257c736520334612c0a94317ff6ab92b45702f
YanMennella <yancorp.tecdev@gmail.com>
2025-03-26 20:56:08 -0300

    objetos

--- aula17/objeto01.js
+++ aula17/objeto01.js
@@ -1,2 +1,8 @@
-let amigo = {}
-console.log(typeof amigo)
+let amigo = {nome: 'José',
+sexo: 'M',
+peso: 85.4,
+engordar(p=0){
+    console.log('Engordou')
+    this.peso += p
+}}
+console.log(amigo.nome)
