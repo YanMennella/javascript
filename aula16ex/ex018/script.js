e0b45e4f2a3081cbac967aef9c4e5ac3e6e78e05
YanMennella <yancorp.tecdev@gmail.com>
2025-04-03 19:16:49 -0300

    Update script.js

--- aula16ex/ex018/script.js
+++ aula16ex/ex018/script.js
@@ -1,5 +1,7 @@
 let num = document.querySelector('input#fnum')
 let lista = document.querySelector('select#flista')
+let res = document.querySelector('div#res')
+let valores = []
 
 function adicionar() {
 
