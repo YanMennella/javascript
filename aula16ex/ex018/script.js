3052029bba695714365f0fda580fd8501e2c0373
YanMennella <yancorp.tecdev@gmail.com>
2025-04-03 19:26:33 -0300

    Update script.js

--- aula16ex/ex018/script.js
+++ aula16ex/ex018/script.js
@@ -3,8 +3,24 @@ let lista = document.querySelector('select#flista')
 let res = document.querySelector('div#res')
 let valores = []
 
+function isNumero(n) {
+    if(Number(n) >= 1 && Number(n) <= 100) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function inLista(n, l) {
+    if (l.indexOf(Number(n)) != -1) {
+        
+    }
+}
+
 function adicionar() {
     if(isNumero(num.value) && !inlista(num.value)) {
-        
+
+    } else {
+        window.alert('Valor inválido ou já encontrado na lista.')
     }
 }
