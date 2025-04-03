54855194c5b1acc7a2889929f78200a318f3a08a
YanMennella <yancorp.tecdev@gmail.com>
2025-04-02 23:17:48 -0300

    Update script.js

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -6,12 +6,16 @@ function contar() {
 
     if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
         res.innerHTML = 'Impossível contar!'
-        window.alert('[ERRO] Faltam dados!')
+        //window.alert('[ERRO] Faltam dados!')
     } else {
-        res.innerHTML = 'Contando: '
+        res.innerHTML = 'Contando: <br>'
         let i = Number(ini.value)
         let f = Number(fim.value)
         let p = Number(passo.value)
+        if (p <= 0) {
+            window.alert('Passo inválido! Considerando PASSO 1')
+            p = 1
+        }
         if (i < f) {
             // Contagem crescente
             for(let c = i; c <= f; c+= p) {
