12a8099c38e71737db3c66a5b8bb38f5bd10d165
YanMennella <yancorp.tecdev@gmail.com>
2025-04-03 23:34:12 -0300

    Update script.js

--- aula16ex/ex018/script.js
+++ aula16ex/ex018/script.js
@@ -21,7 +21,7 @@ function inLista(n, l) {
 
 function adicionar() {
     if(isNumero(num.value) && !inLista(num.value, valores)) {
-    
+        valores.push(Number(num.value))
     } else {
         window.alert('Valor inválido ou já encontrado na lista.')
     }
